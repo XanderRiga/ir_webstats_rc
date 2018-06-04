82fdba14a78467c931fd5cb08a3f390fbeb4ab2e
Fuzzwah <fuzzwah@github.com>
2018-06-04 14:34:39 +1000

    incident bitflags

--- test.py
+++ test.py
@@ -1,29 +1,68 @@
 #!/usr/bin/python
 import sys
-from ir_webstats import constants as ct
-from ir_webstats.client import iRWebStats
-from ir_webstats.util import clean
-
-if (sys.version_info > (3, 0)):
-    user = input('Username: ')
-    password = input('Password: ')
-else:
-    user = raw_input('Username: ')
-    password = raw_input('Password: ')
+from ir_webstats_rc import constants as ct
+from ir_webstats_rc.client import iRWebStats
+from ir_webstats_rc.util import clean
 
 irw = iRWebStats()
-irw.login(user, password)
+irw.login()
 if not irw.logged:
-    print (
-        "Couldn't log in to iRacing Membersite. Please check your credentials")
-    exit()
+    user = input('Username: ')
+    password = input('Password: ')
+    irw.login(user, password)
+    if not irw.logged:
+        print (
+            "Couldn't log in to iRacing Membersite. Please check your credentials")
+        exit()
 
-for i in range(1, 13):
-    sof = 0
-    sof_sessionid = None
-    for res in irw.season_race_sessions(1708, i):
-        if res['strengthoffield'] > sof:
-           sof = res['strengthoffield']
-           sof_sessionid = res['sessionid']
-    if not sof_sessionid == None:
-        print("SOF for week {}: session {} sof {}".format(i, sof_sessionid, sof))
+if irw.logged:
+    inc_flags = {
+        0: "clean",
+        2: "pitted",
+        4: "off_track",
+        8: "black_flag",
+        16: "car_reset",
+        32: "contact",
+        64: "car_contact",
+        128: "lost_control",
+        256: "discontinuity",
+        512: "interpolated_crossing",
+        1024: "clock_smash",
+        2048: "tow"
+    }
+    incs = {
+        "clean": 0,
+        "pitted": 0,
+        "off_track": 0,
+        "black_flag": 0,
+        "car_reset": 0,
+        "contact": 0,
+        "car_contact": 0,
+        "lost_control": 0,
+        "discontinuity": 0,
+        "interpolated_crossing": 0,
+        "clock_smash": 0,
+        "tow": 0,
+    }
+    for lap in irw.event_laps_all(23346289)["lapdata"]:
+        if lap['flags'] == 0:
+            incs["clean"] += 1
+        for k in inc_flags.keys():
+            if lap['flags'] & k:
+                incs[inc_flags[k]] += 1
+
+    print(incs)
+"""
+			if(flags & 2)flags_comments.push("pitted");
+			if(flags & 4)flags_comments.push("off track");
+			if(flags & 8)flags_comments.push("black flag");
+			if(flags & 16)flags_comments.push("car reset");
+			if(flags & 32)flags_comments.push("contact");
+			if(flags & 64)flags_comments.push("car contact");
+			if(flags & 128)flags_comments.push("lost control");
+
+			if(flags & 256)flags_comments.push("discontinuity");
+			if(flags & 512)flags_comments.push("interpolated crossing");
+			if(flags & 1024)flags_comments.push("clock smash");
+			if(flags & 2048)flags_comments.push("tow");
+"""
