c925114033ebd39a8e588e68c191b28bed82f6a7
Xander <xanderriga@live.com>
2020-05-03 10:01:27 -0700

    Create responses module
    This allows me to provide some structure to the responses that iracing
    gives us instead of just dumping json.

---
+++ responses/last_races_stats.py
@@ -0,0 +1,16 @@
+class LastRacesStats:
+    def __init__(self, dict):
+        self.startPos = dict['startPos']
+        self.lapsLed = dict['lapsLed']
+        self.finishPos = dict['finishPos']
+        self.incidents = dict['incidents']
+        self.trackName = dict['trackName']
+        self.date = dict['date']
+        self.time = dict['time']
+        self.winnerName = dict['winnerName']
+        self.winnerID = dict['winnerID']
+        self.clubPoints = dict['clubPoints']
+        self.champPoints = dict['champPoints']
+        self.subsessionID = dict['subsessionID']
+        self.seasonID = dict['seasonID']
+        self.winnerLL = dict['winnerLL']
