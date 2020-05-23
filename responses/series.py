c37bffcee036d742989489a1ff90e092a84ce53f
Xander <xanderriga@live.com>
2020-05-23 09:27:27 -0700

    Add short names for racing series
    Some of these series names are long as hell, so lets shorten some as we
    see them so they can actually fit in discord.

--- responses/series.py
+++ responses/series.py
@@ -1,12 +1,25 @@
 from .car import Car
 
+name_mapping = {
+    'iRacing Street Stock Series - R': 'Street Stock - Rookie',
+    'DIRTcar Street Stock Series - Fixed': 'Dirt Street Stock - Fixed',
+    'Rookie iRacing Rallycross Series': 'Rallycross - Rookie',
+    'iRacing Advanced Legends Cup': 'Advanced Legends Cup'
+}
+
 
 class Series:
     def __init__(self, dict):
         self.seriesId = dict['seriesid']
-        self.name = dict['seriesname']
+        self.name = self.short_name(dict['seriesname'])
         self.categoryId = dict['catid']
         self.cars = list(map(lambda x: Car(x), dict['cars']))
 
     def car_name_list(self):
         return list(map(lambda x: x.name, self.cars))
+
+    def short_name(self, name):
+        if name not in name_mapping:
+            return name
+
+        return name_mapping[name]
